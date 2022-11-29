ade9f01960fd6b94d87c9a3ba01825a050246777
Kazakov Mihail <advancerman@gmail.com>
2022-11-29 20:23:07 +0300

    Initial commit

---
+++ src/main/kotlin/com/example/hw8/Hw8Application.kt
@@ -0,0 +1,11 @@
+package com.example.hw8
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Hw8Application
+
+fun main(args: Array<String>) {
+	runApplication<Hw8Application>(*args)
+}
